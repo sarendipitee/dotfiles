1e93b814ba6f5b0ad2271571207aaa904587d7e2
Saren <saren@dumstruck.com>
2026-05-05 12:13:44 -0700

    move final $PATH re-ordering to zsh to avoid macos path_helper injecting /usr/bin/ above flox

--- packages/shell/.config/zsh/path.sh
+++ packages/shell/.config/zsh/path.sh
@@ -1,5 +1,5 @@
-# Start with minimal sane path without /usr/libexec/path_helper or other crap
-PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+# Tool-specific PATH additions
+# Note: Final PATH ordering is done in .zshrc to override macOS path_helper
 
 #Python
 # export PATH=$PATH:$HOME/Library/Python/3.9/bin
@@ -30,25 +30,6 @@ export PATH=$PATH:$GOROOT/bin
 
 # bun
 export PATH="$BUN_INSTALL/bin:$PATH"
-# [ -s "$BUN_INSTALL/_bun" ] && source "$BUN_INSTALL/_bun"
 
 # Proto - shims enable dynamic version detection for non-interactive shells
 export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH"
-
-# remove /usr/local/bin and /usr/bin
-export PATH=$(echo ":$PATH:" | sed -e "s#:/usr/local/bin:#:#g" -e "s/^://" -e "s/:$//")
-export PATH=$(echo ":$PATH:" | sed -e "s#:/usr/bin:#:#g" -e "s/^://" -e "s/:$//")
-# add /usr/local/bin and /usr/bin in that order
-export PATH="/usr/local/bin:/usr/bin:$PATH"
-
-# Load this first so that we prefer homebrew installed over XCode crap
-export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/opt/homebrew/sbin:$PATH"
-
-# Things I build myself go in here, overrides all other paths
-export PATH=$HOME/.local/bin:$PATH
-export PATH=$HOME/.mine/bin:$PATH
-export PATH=$HOME/.mine/scripts:$PATH
-
-# Flox takes ultimate precedence
-eval "$(flox activate -d $DOTFILES_DIR/packages/flox/global-env -m run)"
