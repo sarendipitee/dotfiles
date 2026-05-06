477dd122b0f3e65414b0cd49a6a16d06c01b431d
Saren <saren@dumstruck.com>
2026-05-06 13:02:13 -0700

    initialize git submodules before stow in create-links.sh

--- scripts/create-links.sh
+++ scripts/create-links.sh
@@ -1,6 +1,10 @@
 script_dir=$(realpath $(dirname $0))
 packages_dir=$(realpath "$script_dir"/../packages)
 
+# Initialize git submodules (for antidote, etc.)
+dotfiles_dir=$(realpath "$script_dir/..")
+cd "$dotfiles_dir" && git submodule update --init --recursive
+
 dir_list="$packages_dir/*"
 
 for dir in $dir_list; do
