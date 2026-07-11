d4a9032888285380b027d06c6d35fb896e22e4e9
Saren <saren@dumstruck.com>
2026-07-10 17:15:10 -0700

    Make <leader>E reveal and highlight current file like <D-E>

--- packages/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ packages/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -67,5 +67,21 @@ return {
       end,
       desc = "Open/Focus NeoTree (Root Dir)",
     },
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root(), reveal = true })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), reveal = true })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
   },
 }
